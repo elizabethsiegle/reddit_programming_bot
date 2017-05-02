3c11206df5925e91b287df78fb64e26943497326
Elizabeth Siegle <lizzie.siegle@gmail.com>
2017-05-01 23:37:41 -0400

    readme, clean up code

--- Main.hs
+++ Main.hs
@@ -53,8 +53,25 @@ import Control.Monad.IO.Class
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 
-randIntHelper:: Int -> Int
-randIntHelper x = 2 * x
+--slack bots: auto site testing, news, phone call, see social
+-- media info about people, send screenshots to GitHub, Trello,
+-- etc,time tracking/allocating, shop, find apps, more
+
+-- slack commands: mute/unmute channel, set reminders, list
+-- users, priv. msg, send/search gifs, search google imgs
+-- = used to increase productivity, access apps/news/etc
+-- from within Slack, automate tasks
+-- use bot when user involved in process (approval, q+a)
+
+--ngrok creates secure public URL (https://yourapp.ngrok.io) 
+--to local webserver on your machine.
+--Inspect HTTP traffic flowing over your tunnel. Then, replay
+--webhook reqs w/ 1 click 2 iterate quickly while staying in context.
+
+--readSlackFile* reads in an incoming Slack webhook letting 
+--external sources (in this case, the bot) post messages into 
+--Slack. It uses normal HTTP requests with a JSON payload 
+--including the message text, but there are additional options, too. 
 
 readSlackFile :: FilePath -> IO Text
 readSlackFile filename =
@@ -72,25 +89,32 @@ parseText text = case T.strip text of
 liftMaybe :: Maybe a -> IO a
 liftMaybe = maybe mzero return
 
+-- printPost* formats the Reddit post, converting it into 
+--Text and creating an external link to take you to the 
+--trending Reddit post. 
 printPost :: Post -> T.Text
 printPost post = do
   title post <> "\n" <> (T.pack . show . created $ post) <> "\n" <> "http://reddit.com"<> permalink post <> "\n" <> "Score: " <> (T.pack . show . score $ post)
 
+--searches for Hot programming posts given input (ie haskell)
 findQPosts:: Text -> RedditT IO PostListing
 findQPosts c = search (Just $ R "programming") (Options Nothing (Just 0)) Hot c
 
+--gets Reddit posts, and creates the message that will be 
+--posted to Slack. 
 messageOfCommandReddit :: Command -> IO Network.Linklater.Message
 messageOfCommandReddit (Command "reddit" user channel (Just text)) = do
   query <- liftMaybe (parseText text)
   posts <- runRedditAnon (findQPosts query)
-  --num <- randomIO :: IO Int
   case posts of
     Right posts' ->
        return (messageOf [FormatAt user, FormatString (T.intercalate "\n\n". Prelude.map printPost $ contents posts')]) where
         messageOf =
           FormattedMessage(EmojiIcon "gift") "redditbot" channel
           
-
+--calls *messageOfCommandReddit*, which actually posts the 
+--message to Slack after running *stack build*, then *stack 
+--exec redditbot*, and then opening up a Ngrok tunnel at the same port.
 redditify :: Maybe Command -> IO Text
 redditify Nothing =
   return "Unrecognized Slack request!"
@@ -104,42 +128,10 @@ redditify(Just command) = do
     (False,  m) -> do
       _ <- say m config
       return ""
-    --(False, Nothing) ->
-    --  return "ugh"
     _ ->
       return ""
   where
     debug = False
-
---messageOfCommandHackerNews :: Command -> IO Network.Linklater.Message
---messageOfCommandHackerNews (Command "hackernews" user channel (Just text)) = do
---  mgr <- newManager tlsManagerSettings
---  case mgr of
---    Right (getTopStories mgr) ->
---    return (messageOf [FormatAt user, FormatString (getTopStories mgr)])
---      where 
---        messageOf =
---          FormattedMessage(EmojiIcon "gift") "hackernews" channel
-
---hnify:: Maybe Command -> IO Text
---hnify Nothing = 
---  return "Unrecognized Slack request"
-
---hnify (Just command) = do
---  Prelude.putStrLn ("+ Incoming command: " <> show command)
---  message <- (messageOfCommandHackerNews) command
---  config <- configIO
---  --putStrLn ("+ Outgoing message: " <> show (message))
---  case (debug, message) of
---    (False,  m) -> do
---      _ <- say m config
---      return ""
---    --(False, Nothing) ->
---    --  return "ugh"
---    _ ->
---      return ""
---  where
---    debug = False
 
 main :: IO ()
 main = do
